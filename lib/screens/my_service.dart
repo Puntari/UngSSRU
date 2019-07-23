5e0bfdc8d459fb4fde34c589d0624d38d77a1f81
puntari <puntari@abc.com>
2019-07-23 11:48:19 +0700

    Register Success

--- lib/screens/my_service.dart
+++ lib/screens/my_service.dart
@@ -1,4 +1,8 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart' as prefix0; //3
 
 class MyService extends StatefulWidget {
   @override
@@ -7,21 +11,69 @@ class MyService extends StatefulWidget {
 
 class _MyServiceState extends State<MyService> {
 //Explicit
+  FirebaseAuth firebaseAuth = FirebaseAuth.instance;
+  String namestring = '';
 
 //Method
+  @override
+  void initState() {
+    super.initState();
+    findDisplayName();
+  }
+
+  Future<void> findDisplayName() async {
+    FirebaseUser firebaseUser = await firebaseAuth.currentUser();
+    setState(() {
+      namestring = firebaseUser.displayName;
+    });
+    print('name = $namestring');
+  } //เทรด
+
   Widget showDrawMenu() {
     return Drawer(
       child: ListView(
         children: <Widget>[
           headMenu(),
+          singOutAnExit(),
         ],
       ),
     );
   }
 
+  Widget singOutAnExit() {
+    return ListTile(
+      leading: Icon(
+        Icons.exit_to_app,
+        size: 35.0,
+        color: Colors.red,
+      ),
+      title: Text(
+        'Sign Out & Exit',
+        style: TextStyle(color: Colors.red, fontSize: 18.0),
+      ),onTap: (){
+        mySignOut(); //คอร
+
+      }//ถ้าคลิกsign out ทำงานใน ontap,
+    );
+  }
+
+  Future<void> mySignOut()async{
+
+    await firebaseAuth.signOut().then((response){
+      exit(0);
+    });
+
+  }//สร้าง-->คอร
+
   Widget headMenu() {
     return DrawerHeader(
-      decoration: BoxDecoration(),
+      decoration: BoxDecoration(
+        gradient: RadialGradient(
+          colors: [Colors.white, Colors.indigo[300]],
+          radius: 1.0,
+          center: Alignment.center,
+        ),
+      ), //พื้นหลังด้านกล่องบน
       child: Column(
         children: <Widget>[
           Container(
@@ -32,14 +84,14 @@ class _MyServiceState extends State<MyService> {
           Text(
             'My Hell',
             style: TextStyle(
-              color: Colors.lime,
+              color: Colors.yellowAccent,
               fontSize: 25.0,
             ),
-          ),//ชื่อแอป
+          ), //ชื่อแอป
           Text(
-            'Login by ...',
-            style: TextStyle(color: Colors.amber),
-          ) 
+            'Login by $namestring',
+            style: TextStyle(color: Colors.teal),
+          )
         ],
       ),
     );
