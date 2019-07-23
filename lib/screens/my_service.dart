e1b6039d3f57b311c10a565e2ea74164880afc57
puntari <puntari@abc.com>
2019-07-23 10:31:20 +0700

    Create HeadDraw

--- lib/screens/my_service.dart
+++ lib/screens/my_service.dart
@@ -6,6 +6,45 @@ class MyService extends StatefulWidget {
 }
 
 class _MyServiceState extends State<MyService> {
+//Explicit
+
+//Method
+  Widget showDrawMenu() {
+    return Drawer(
+      child: ListView(
+        children: <Widget>[
+          headMenu(),
+        ],
+      ),
+    );
+  }
+
+  Widget headMenu() {
+    return DrawerHeader(
+      decoration: BoxDecoration(),
+      child: Column(
+        children: <Widget>[
+          Container(
+            width: 70.0,
+            height: 70.0,
+            child: Image.asset('images/logo.png'),
+          ), //โชว์รูป
+          Text(
+            'My Hell',
+            style: TextStyle(
+              color: Colors.lime,
+              fontSize: 25.0,
+            ),
+          ),//ชื่อแอป
+          Text(
+            'Login by ...',
+            style: TextStyle(color: Colors.amber),
+          ) 
+        ],
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -13,6 +52,7 @@ class _MyServiceState extends State<MyService> {
         title: Text('My Service'),
       ),
       body: Text('body'),
+      drawer: showDrawMenu(),
     );
   }
 }
